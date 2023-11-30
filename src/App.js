39db563b7461f806a133edde6d29b152511c8173
joaoAaf <joao.anderson.af@gmail.com>
2023-11-30 15:54:38 -0300

    alteração página inicial

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DevOps Básico IT Talent - Mudanças aula 26-04-2023
+          DevOps Básico IT Talent - Prática CI/CD I 30-11-2023
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/joaoAaf/pratica-ci-cd-1"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn DevOps
+          Code on GitHub
         </a>
       </header>
     </div>
